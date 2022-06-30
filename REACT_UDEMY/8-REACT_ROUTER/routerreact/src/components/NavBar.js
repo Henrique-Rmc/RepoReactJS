c59927445d4501e389e5b0234028d6d61670bdd3
Henrique-Rmc <paulo.cruz@ccc.ufcg.edu.br>
2022-06-30 16:12:48 -0300

    Base React Router Completa

--- REACT_UDEMY/8-REACT_ROUTER/routerreact/src/components/NavBar.js
+++ REACT_UDEMY/8-REACT_ROUTER/routerreact/src/components/NavBar.js
@@ -1,18 +1,19 @@
 //2- Links com React Router
 import './NavBar.css'
 
-import {Link} from "react-router-dom"
+import {Link, NavLink, navLink} from "react-router-dom"
 
 const NavBar = () => {
   return <nav>
 
     {/*Também poderia ser escrito como <a href = '/'>Home</a>
-        entretando, o href faz um reload da página e o Link não  */}
-
+        entretando, o href faz um reload da página e o Link não  
     <Link to ='/'>Home</Link>
-    
+    <Link to= "/about">Sobre</Link>*/}
 
-    <Link to= "/about">Sobre</Link>
+  <NavLink to = "/">Home</NavLink>
+  <NavLink to = "/about">Sobre</NavLink>
+
 
 
   </nav>
---
+++ REACT_UDEMY/8-REACT_ROUTER/routerreact/src/pages/Info.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
+
+
+const Info = () => {
+    const {id} = useParams()
+
+  return (
+    <div>
+        <h1>Informações adicionais sobre o produto: {id}</h1>
+    </div>
+  )
+}
+
+export default Info
