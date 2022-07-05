b71c68aab2b5ddc02fb82c27a5986f270c2a7ffe
Henrique-Rmc <paulo.cruz@ccc.ufcg.edu.br>
2022-07-05 12:11:33 -0300

    ...

--- PP_MercadoPlantas/mercadoplantas/src/components/Navbar.js
+++ PP_MercadoPlantas/mercadoplantas/src/components/Navbar.js
@@ -1,10 +1,28 @@
 import { NavLink } from "react-router-dom"
 
+import styles from './Navbar.module.css'
+
+
 const Navbar = () => {
-  return <nav>
-    <NavLink to= '/'>Home</NavLink>
-    <NavLink to = '/about'>Sobre</NavLink>
-    <NavLink to = '/plants'>Catálogo</NavLink>
+  return <nav className={styles.navbar}>
+
+    <NavLink to = "/" className={styles.brand}>Mercado<span>PlantasLivres</span></NavLink>
+      <ul>
+
+        <li>
+        <NavLink to= '/'className={({isActive})=>(isActive ? styles.active : "")} >Home</NavLink>
+        </li>
+
+        <li>
+        <NavLink to = '/about'className={({isActive})=>(isActive ? styles.active : "")} >Sobre</NavLink>
+        </li>
+
+        <li>
+        <NavLink to = '/plants'className={({isActive})=>(isActive ? styles.active : "")} >Catálogo</NavLink>
+        </li>
+        
+      </ul>
+
     </nav>
 }
 
--- PP_MercadoPlantas/mercadoplantas/src/pages/Home/Home.js
+++ PP_MercadoPlantas/mercadoplantas/src/pages/Home/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
+
 
 const Home = () => {
   return (
     <div>
-        <h1>Home</h1>
+        <h1>Mercado PlantasLivres</h1>
+
     </div>
   )
 }
